99d6ee3288603ab7ddcf7185aae65229b24c911d
Sylvain Chateau <git@sylvain-chateau.com>
2024-09-09 19:35:26 +0200

    SECRET is mandatory and .env should not be inside container (#2)

--- utils/init/index.js
+++ utils/init/index.js
@@ -5,6 +5,10 @@ const initSecret = require('./initSecret');
 const normalizeTheme = require('./normalizeTheme');
 
 const initApp = async () => {
+  if (!process.env.SECRET) {
+    console.error('ERROR: SECRET key is missing in environment variables');
+    process.exit(1);
+  }
   initDockerSecrets();
   initSecret();
   await initFiles();
